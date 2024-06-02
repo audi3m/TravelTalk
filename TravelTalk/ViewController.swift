675c5907f1770c0b0ee6b00f6acf8348511ac2ae
ray <audi3m@naver.com>
2024-06-02 17:32:51 +0900

    Initial Commit

---
+++ TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by J Oh on 6/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
